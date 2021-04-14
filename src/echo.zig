366ac7e8ec25aa11402fe7806ce3a085eb8b9825
zuyoutoki <zuyoutoki@oki.moe>
2021-04-14 17:05:52 -0400

    I can't spell, and this new separator should look better

--- src/echo.zig
+++ src/echo.zig
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2021 Zuyoutoki
 // This file is part of [zig-utils](https://github.com/Zuyoutoki/zig-utils/),
-// which is MIT license. The MIT license requires this copyright notice to be
-// included in all copies and substancial portions of the software.
+// which is MIT licensed. The MIT license requires this copyright notice to be
+// included in all copies and substantial portions of the software.
 const std = @import("std");
 const allocator = std.heap.page_allocator;
 
