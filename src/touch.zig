931569b22c398cf32ff9a846bf8c554ded70397a
zuyoutoki <zuyoutoki@oki.moe>
2021-04-18 18:34:37 -0400

    `touch`: rewrite, now with help

--- src/touch.zig
+++ src/touch.zig
@@ -6,108 +6,105 @@
 const std = @import("std");
 const allocator = std.heap.page_allocator;
 
-pub fn main() !void {
-    const stderr = std.io.getStdErr().writer();
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+const usage =
+    \\Usage: touch [options] <file...>
+    \\
+    \\Options:
+    \\  -h, --help          Print this help and exit
+    \\  -a                  Change only the access time
+    \\  -c, --no-create     Do not create any file
+    \\  -m                  Change only the modification time
+    \\
+;
 
-    if (args.len < 2) {
-        try stderr.print("usage: touch FILE...\n", .{});
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
+    const all_args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, all_args);
+
+    var no_create: bool = false;
+    var atime_only: bool = false;
+    var mtime_only: bool = false;
+
+    const args = all_args[1..];
+    var i: usize = 0;
+    while (i < args.len) : (i += 1) {
+        const arg = args[i];
+        if (std.mem.startsWith(u8, arg, "-")) {
+            if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
+                _ = try stdout.print("{s}", .{usage});
+                return;
+            } else if (std.mem.eql(u8, arg, "-a")) {
+                if (mtime_only) {
+                    _ = try stderr.print("error: -a and -m cannot be used at the same time\n", .{});
+                    return;
+                }
+                atime_only = true;
+            } else if (std.mem.eql(u8, arg, "-c") or std.mem.eql(u8, arg, "--no-create")) {
+                no_create = true;
+            } else if (std.mem.eql(u8, arg, "-m")) {
+                if (atime_only) {
+                    _ = try stderr.print("error: -a and -m cannot be used at the same time\n", .{});
+                    return;
+                }
+                mtime_only = true;
+            } else {
+                _ = try stderr.print("error: Unknown option: {s}\n", .{arg});
+                return;
+            }
+        } else break;
+    }
+
+    if (args.len == i) {
+        try stderr.print("{s}", .{usage});
         return;
     }
-    for (args[1..]) |arg| {
+    while (i < args.len) : (i += 1) {
+        const arg = args[i];
         var f_path = try std.fs.path.resolve(allocator, &[_][]const u8{arg});
         var cwd = std.fs.cwd();
+        var f_file: std.fs.File = undefined;
 
-        var f_file = cwd.createFile(f_path, .{ .truncate = false }) catch |e| switch (e) {
-            error.SharingViolation => {
-                try stderr.print("error: SharingViolation for '{s}'\n", .{arg});
-                return;
-            },
-            error.PathAlreadyExists => {
-                try stderr.print("error: PathAlreadyExists for '{s}'\n", .{arg});
-                return;
-            },
-            error.FileNotFound => {
-                try stderr.print("error: FileNotFound for '{s}' \n", .{arg});
-                return;
-            },
-            error.AccessDenied => {
-                try stderr.print("error: AccessDenied for '{s}'\n", .{arg});
-                return;
-            },
-            error.PipeBusy => {
-                try stderr.print("error: PipeBusy for '{s}'\n", .{arg});
-                return;
-            },
-            error.NameTooLong => {
-                try stderr.print("error: NameTooLong for '{s}'\n", .{arg});
-                return;
-            },
-            error.InvalidUtf8 => {
-                try stderr.print("error: InvalidUtf8 for '{s}'\n", .{arg});
-                return;
-            },
-            error.BadPathName => {
-                try stderr.print("error: BadPathName for '{s}'\n", .{arg});
-                return;
-            },
-            error.Unexpected => {
-                try stderr.print("error: Unexpected for '{s}'\n", .{arg});
-                return;
-            },
-            error.SymLinkLoop => {
-                try stderr.print("error: SymLinkLoop for '{s}'\n", .{arg});
-                return;
-            },
-            error.ProcessFdQuotaExceeded => {
-                try stderr.print("error: ProcessFdQuotaExceeded for '{s}'\n", .{arg});
-                return;
-            },
-            error.SystemFdQuotaExceeded => {
-                try stderr.print("error: SystemFdQuotaExceeded for '{s}'\n", .{arg});
-                return;
-            },
-            error.NoDevice => {
-                try stderr.print("error: NoDevice for '{s}'\n", .{arg});
-                return;
-            },
-            error.SystemResources => {
-                try stderr.print("error: SystemResources for '{s}'\n", .{arg});
-                return;
-            },
-            error.FileTooBig => {
-                try stderr.print("error: FileTooBig for '{s}'\n", .{arg});
-                return;
-            },
-            error.IsDir => {
-                try stderr.print("error: IsDir for '{s}'\n", .{arg});
-                return;
-            },
-            error.NoSpaceLeft => {
-                try stderr.print("error: NoSpaceLeft for '{s}'\n", .{arg});
-                return;
-            },
-            error.NotDir => {
-                try stderr.print("error: NotDir for '{s}'\n", .{arg});
-                return;
-            },
-            error.DeviceBusy => {
-                try stderr.print("error: DeviceBusy for '{s}'\n", .{arg});
-                return;
-            },
-            error.FileLocksNotSupported => {
-                try stderr.print("error: FileLocksNotSupported for '{s}'\n", .{arg});
-                return;
-            },
-            error.WouldBlock => {
-                try stderr.print("error: WouldBlock for '{s}'\n", .{arg});
-                return;
-            },
-        };
+        if (no_create) {
+            f_file = cwd.openFile(f_path, .{}) catch |e| switch (e) {
+                error.PathAlreadyExists => {
+                    try stderr.print("error: PathAlreadyExists for '{s}'\n", .{arg});
+                    return;
+                },
+                error.FileNotFound => {
+                    try stderr.print("error: FileNotFound for '{s}'\n", .{arg});
+                    return;
+                },
+                error.AccessDenied => {
+                    try stderr.print("error: AccessDenied for '{s}'\n", .{arg});
+                    return;
+                },
+                else => return e,
+            };
+        } else {
+            f_file = cwd.createFile(f_path, .{ .truncate = false }) catch |e| switch (e) {
+                error.FileNotFound => {
+                    try stderr.print("error: FileNotFound for '{s}'\n", .{arg});
+                    return;
+                },
+                error.AccessDenied => {
+                    try stderr.print("error: AccessDenied for '{s}'\n", .{arg});
+                    return;
+                },
+                else => return e,
+            };
+        }
         defer f_file.close();
 
         var cur_time = std.time.nanoTimestamp();
-        try f_file.updateTimes(cur_time, cur_time);
+        var f_stat = try f_file.stat();
+        if (atime_only) {
+            try f_file.updateTimes(cur_time, f_stat.mtime);
+        } else if (mtime_only) {
+            try f_file.updateTimes(f_stat.atime, cur_time);
+        } else {
+            try f_file.updateTimes(cur_time, cur_time);
+        }
     }
 }
