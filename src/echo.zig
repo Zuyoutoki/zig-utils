3a0c5bc8cb841c0fc3dd20efdb0fd4fed43bcd79
zuyoutoki <zuyoutoki@oki.moe>
2021-04-18 17:37:30 -0400

    `echo`: rewrite, now with help

--- src/echo.zig
+++ src/echo.zig
@@ -6,15 +6,44 @@
 const std = @import("std");
 const allocator = std.heap.page_allocator;
 
+const usage =
+    \\Usage: echo [options] [string...]
+    \\
+    \\Options:
+    \\  -h, --help      Print this help and exit
+    \\  -n              Do not output a trailing newline
+    \\
+;
+
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+    const stderr = std.io.getStdErr().writer();
+    const all_args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, all_args);
 
-    for (args[1..]) |arg| {
-        _ = try stdout.write(arg);
-        _ = try stdout.write(" ");
+    var trailing_newline: bool = true;
+
+    const args = all_args[1..];
+    var i: usize = 0;
+    while (i < args.len) : (i += 1) {
+        const arg = args[i];
+        if (std.mem.startsWith(u8, arg, "-")) {
+            if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
+                _ = try stdout.print("{s}", .{usage});
+                return;
+            } else if (std.mem.eql(u8, arg, "-n")) {
+                trailing_newline = false;
+            } else {
+                _ = try stderr.print("error: Unknown option: {s}", .{arg});
+                return;
+            }
+        } else break;
     }
-    _ = try stdout.write("\n");
+
+    while (i < args.len) : (i += 1) {
+        _ = try stdout.write(args[i]);
+        if (i + 1 < args.len) _ = try stdout.write(" ");
+    }
+    if (trailing_newline) _ = try stdout.write("\n");
     return;
 }
